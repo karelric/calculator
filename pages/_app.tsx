c1a3e289aa656aded144ef71ae19d471059c8ef9
Karel Ricardo <krel0024@icloud.com>
2025-12-04 21:37:49 -0600

    upgrade next version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import { Rubik } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { Rubik } from "next/font/google";
 import Head from "next/head";
 import "../styles/globals.css";
 
